02721a0a6b879d64326f243b383b22efc68687a0
iamfaget <iamfaget@shitmail.me>
2013-09-17 14:47:49 +0600

    KUZYA_SUPERB_APPNAME
    KUZYA_SUPERB_APPNAME_DESCRIPTION

---
+++ KUZYA_SUPERB_APPNAME/KUZYA_SUPERB_APPNAME/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+        
+namespace KUZYA_SUPERB_APPNAME
+{    public partial class Form1 : Form
+    {   public Form1()
+        {   InitializeComponent();
+        }
+    }
+}
+
+void    main ()
+{    double  a,b;
+}
