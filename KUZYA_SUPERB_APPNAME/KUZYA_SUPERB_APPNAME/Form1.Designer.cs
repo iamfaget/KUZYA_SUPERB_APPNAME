29ca7fe0788ab614d8361d2ab69a20d79b0b6b67
i_LUV_SOCIAL_NETWORKS <iamfaget@shitmail.me>
2013-10-01 15:49:18 +0600

    dasda
    sadasdas

--- KUZYA_SUPERB_APPNAME/KUZYA_SUPERB_APPNAME/Form1.Designer.cs
+++ KUZYA_SUPERB_APPNAME/KUZYA_SUPERB_APPNAME/Form1.Designer.cs
@@ -28,9 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
             this.Text = "Form1";
+            this.UseWaitCursor = true;
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick_1);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- KUZYA_SUPERB_APPNAME/KUZYA_SUPERB_APPNAME/Form1.cs
+++ KUZYA_SUPERB_APPNAME/KUZYA_SUPERB_APPNAME/Form1.cs
@@ -8,13 +8,25 @@ using System.Text;
 using System.Windows.Forms;
         
 namespace KUZYA_SUPERB_APPNAME
-{    public partial class Form1 : Form
+{   public partial class Form1 : Form
     {   public Form1()
         {   InitializeComponent();
         }
-    }
-}
 
-void    main ()
-{    double  a,b;
+    private void Form1_MouseDown(object sender, MouseEventArgs e)
+    {
+        this.Text = "CLOSING NOW";
+    }
+    
+    private void Form1_MouseMove(object sender, MouseEventArgs e)
+    {
+        this.Text = "X= " + Convert.ToString(e.X) + " Y= " + Convert.ToString(e.Y);
+    }
+
+    private void Form1_MouseClick_1(object sender, MouseEventArgs e)
+    {
+        this.Close();
+    }
+
+    }
 }
