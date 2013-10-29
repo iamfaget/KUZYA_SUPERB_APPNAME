90d4bf931c0e5679bc9f155e50496d051bf29b4e
i_LUV_SOCIAL_NETWORKS <iamfaget@shitmail.me>
2013-10-29 15:26:15 +0600

    blaw-blaw

--- KUZYA_SUPERB_APPNAME/KUZYA_SUPERB_APPNAME/Form1.Designer.cs
+++ KUZYA_SUPERB_APPNAME/KUZYA_SUPERB_APPNAME/Form1.Designer.cs
@@ -28,25 +28,193 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Font = new System.Drawing.Font("Palatino Linotype", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Margin = new System.Windows.Forms.Padding(1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "EXIT";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.UseWaitCursor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.radioButton1.Location = new System.Drawing.Point(12, 32);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(68, 28);
+            this.radioButton1.TabIndex = 3;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Line";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.UseWaitCursor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.radioButton2.Location = new System.Drawing.Point(12, 66);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(81, 28);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "Cross";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.UseWaitCursor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.radioButton3.Location = new System.Drawing.Point(12, 100);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(116, 28);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.Text = "Whatever";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.UseWaitCursor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(129, 571);
+            this.panel1.TabIndex = 6;
+            this.panel1.UseWaitCursor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.radioButton4);
+            this.panel2.Controls.Add(this.radioButton5);
+            this.panel2.Controls.Add(this.radioButton6);
+            this.panel2.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.panel2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.panel2.Location = new System.Drawing.Point(255, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(5050, 567);
+            this.panel2.TabIndex = 7;
+            this.panel2.UseWaitCursor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Checked = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.radioButton4.Location = new System.Drawing.Point(12, 32);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(68, 28);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Line";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.UseWaitCursor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton5.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.radioButton5.Location = new System.Drawing.Point(12, 66);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(81, 28);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.Text = "Cross";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.UseWaitCursor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton6.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.radioButton6.Location = new System.Drawing.Point(12, 100);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(116, 28);
+            this.radioButton6.TabIndex = 5;
+            this.radioButton6.Text = "Whatever";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.UseWaitCursor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.ClientSize = new System.Drawing.Size(790, 571);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "Form1";
             this.ShowInTaskbar = false;
-            this.Text = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "cross hard working";
+            this.TopMost = true;
             this.UseWaitCursor = true;
-            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick_1);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
-            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            //this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
     }
 }
 
--- KUZYA_SUPERB_APPNAME/KUZYA_SUPERB_APPNAME/Form1.cs
+++ KUZYA_SUPERB_APPNAME/KUZYA_SUPERB_APPNAME/Form1.cs
@@ -8,25 +8,154 @@ using System.Text;
 using System.Windows.Forms;
         
 namespace KUZYA_SUPERB_APPNAME
-{   public partial class Form1 : Form
-    {   public Form1()
-        {   InitializeComponent();
-        }
+{
+// 1st
+public partial class Form1 : Form
+{
+    Graphics    gr;
+    Point   p1;
+    Point   p2;
+
+    List<Points> Pointslist = new List<Points>();
+    private System.Drawing.Drawing2D.GraphicsPath mousePath = new System.Drawing.Drawing2D.GraphicsPath();
+
+    public Form1()
+    {   
+        InitializeComponent();
+        this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+        this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+        this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+    }
+
+    private void Form1_Paint(object sender, PaintEventArgs e)
+    {
+        Pen penselect = System.Drawing.Pens.Green;
+        e.Graphics.DrawLine(penselect, p1, p2);
+    }
 
     private void Form1_MouseDown(object sender, MouseEventArgs e)
     {
-        this.Text = "CLOSING NOW";
-    }
-    
-    private void Form1_MouseMove(object sender, MouseEventArgs e)
-    {
-        this.Text = "X= " + Convert.ToString(e.X) + " Y= " + Convert.ToString(e.Y);
+        Point mouseDownLocation = new Point(e.X/*+this.Location.X*/, e.Y/*+this.Location.Y*/);
+        //mousePath.Dispose();
+        //mousePath = new System.Drawing.Drawing2D.GraphicsPath();
+        //this.Invalidate();
+        mousePath.AddLine(mouseDownLocation, mouseDownLocation);
+
+        p1.X = e.X;
+        p1.Y = e.Y;
     }
 
-    private void Form1_MouseClick_1(object sender, MouseEventArgs e)
+    private void Form1_MouseUp(object sender, MouseEventArgs e)
+    {
+        p2.X = e.X;
+        p2.Y = e.Y;
+
+        Invalidate();
+
+        //System.Drawing.Point mouseUpLocation = new System.Drawing.Point(e.X/*+this.Location.X*/, e.Y/*+this.Location.Y*/);
+        //mousePath.AddLine(mouseUpLocation, mouseUpLocation);
+    }
+
+/*    private void Form1_MouseMove(object sender, MouseEventArgs e, PaintEventArgs f)
+    {
+        if (radioButton1.Checked)
+        {
+            this.Text = "Drawing a LINE: (" + Convert.ToString(e.X - panel1.Width) + ", " + Convert.ToString(e.Y) + ")";
+            int mouseX = e.X;// + this.Location.X;
+            int mouseY = e.Y;// + this.Location.Y;
+            //this.Text = "X";
+            mousePath.AddLine(mouseX, mouseY, mouseX, mouseY);
+        }
+
+        if (radioButton2.Checked)
+        {
+            this.Text = "Drawing a CROSS: (" + Convert.ToString(e.X - panel1.Width) + ", " + Convert.ToString(e.Y) + ")";
+            int mouseX = e.X;// + this.Location.X;
+            int mouseY = e.Y;// + this.Location.Y;
+//            Cross(mouseX, mouseY, f);
+        }
+
+        if (radioButton3.Checked)
+            this.Text = "Drawing a SUMFIN: ("+Convert.ToString(e.X-panel1.Width)+", "+Convert.ToString(e.Y)+")";
+    }
+*/
+
+    private void button1_Click(object sender, EventArgs e)
     {
         this.Close();
     }
 
+    private void button1_MouseDown(object sender, MouseEventArgs e)
+    {
+        this.Text = "CLOSING NOW";
     }
+
+    private void radioButton1_CheckedChanged(object sender, EventArgs e)
+    {
+        radioButton2.Checked = false;
+        radioButton3.Checked = false;
+    }
+
+    private void radioButton2_CheckedChanged(object sender, EventArgs e)
+    {
+        radioButton1.Checked = false;
+        radioButton3.Checked = false;
+    }
+
+    private void radioButton3_CheckedChanged(object sender, EventArgs e)
+    {
+        radioButton1.Checked = false;
+        radioButton2.Checked = false;
+    }
+
+}
+// 2nd
+public class Point
+{
+    Point p1;
+    Pen p = new Pen(Color.Black, 2);
+    public Point(Point p1)
+    {
+        this.p1 = p1;
+    }
+    public void drawwith(Graphics e)
+    {
+        e.DrawLine(p, p1.X + 5, p1.Y + 5, p1.X - 5, p1.Y - 5);
+    }
+
+}
+
+public class Cross
+{
+    Point p1;
+    Pen p = new Pen(Color.Black, 2);
+    public Cross(Point p1)
+    {
+        this.p1=p1;
+    }
+    public void drawwith(Graphics e)
+    {
+        e.DrawLine(p, p1.X + 5, p1.Y + 5, p1.X - 5, p1.Y - 5);
+        //e.DrawLine(p, p2.X + 5, p2.Y + 5, p2.X - 5, p2.Y - 5);
+    }
+}
+
+public class Line
+{
+    Point p1, p2;
+    Pen p = new Pen(Color.Black, 2);
+    public Line(Point p1, Point p2)
+    {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+    public void drawwith(Graphics e)
+    {
+        Point p1 = new Point(100, 100);
+        Point p2 = new Point(500, 100);
+
+        e.DrawLine(p, p1, p2);
+    }
+}
+
 }
